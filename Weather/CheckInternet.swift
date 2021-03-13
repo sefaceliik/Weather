4c9f9ac8e7e9597be8d5c0bed5a8f7f7c0428a2a
Sefa MacBook Pro <>
2021-03-13 22:22:52 +0300

    first commit

---
+++ Weather/CheckInternet.swift
@@ -0,0 +1,8 @@
+//
+//  CheckInternet.swift
+//  Weather
+//
+//  Created by Sefa MacBook Pro on 13.03.2021.
+//
+
+import Foundation
---
+++ Weather/ChosenCity.swift
@@ -0,0 +1,8 @@
+//
+//  ChosenCity.swift
+//  Weather
+//
+//  Created by Sefa MacBook Pro on 12.03.2021.
+//
+
+import Foundation
